b80ff48c386ba9a43baeed5c3904cea548d7af48
Daniel <danielmuriel1@hotmail.com>
2026-07-27 15:11:01 -0500

    bump: update app version to 0.6.0

--- internal/gui/about_page.go
+++ internal/gui/about_page.go
@@ -9,7 +9,7 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-const version = "0.5.0"
+const version = "0.6.0"
 
 func (ui *settingsUI) buildAboutPage() fyne.CanvasObject {
 	bold := fyne.TextStyle{Bold: true}
