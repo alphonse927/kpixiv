22c7e8f421724a86f92cc611926d09f7d0e46c1c
Daniel <danielmuriel1@hotmail.com>
2026-07-29 08:40:07 -0500

    refactor: remove required validation for download directory in settings UI

--- internal/gui/settings_page.go
+++ internal/gui/settings_page.go
@@ -80,11 +80,8 @@ func (ui *settingsUI) buildSettingsPage() fyne.CanvasObject {
 		container.NewPadded(
 			container.NewVBox(
 				section("Storage"),
-				widget.NewForm(&widget.FormItem{
-					Text:     "Download Directory",
-					Widget:   container.NewBorder(nil, nil, nil, browse, ui.downloadPath),
-					Required: true,
-				}),
+				widget.NewLabel("Download Directory"),
+				container.NewBorder(nil, nil, nil, browse, ui.downloadPath),
 				other,
 			),
 		),
